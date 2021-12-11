56b595ff0854fbe95cfecbfffdbafe8c30e75370
migueliiin <91721552+migueliiin@users.noreply.github.com>
2021-12-11 19:23:56 +0100

    piezas implementadas

---
+++ Ajedrez.py
@@ -0,0 +1,16 @@
+piezas_bl={
+    'torre': chr(0x2656),
+    'caballo': chr(0x2658),
+    'alfil': chr(0x2657),
+    'reina': chr(0x2655),
+    'rey': chr(0x2654),
+    'peon': chr(0x2659)
+}
+piezas_ne={
+    'torre': chr(0x265C),
+    'caballo': chr(0x265E),
+    'alfil': chr(0x265D),
+    'reina': chr(0x265B),
+    'rey': chr(0x265A),
+    'peon': chr(0x265F)
+}
