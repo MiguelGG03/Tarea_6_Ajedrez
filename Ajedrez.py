fe5291c97ebdb7fdf6eeb4b70028725126701790
migueliiin <91721552+migueliiin@users.noreply.github.com>
2021-12-11 19:33:28 +0100

    funcion piezas_negras

--- Ajedrez.py
+++ Ajedrez.py
@@ -14,3 +14,22 @@ piezas_ne={
     'rey': chr(0x265A),
     'peon': chr(0x265F)
 }
+tablero = []
+
+for i in range(0,8):
+    for j in range(0,8):
+        tablero.append(' ')
+
+def colocar_negras():
+    #Colocar piezas negras
+    tablero[0][0] = piezas_ne['torre']
+    tablero[0][1] = piezas_ne['caballo']
+    tablero[0][2] = piezas_ne['alfil']
+    tablero[0][3] = piezas_ne['reina']
+    tablero[0][4] = piezas_ne['rey']
+    tablero[0][5] = piezas_ne['alfil']
+    tablero[0][6] = piezas_ne['caballo']
+    tablero[0][7] = piezas_ne['torre']
+    #Colocar peones negros
+    for i in range(0,8):
+        tablero[1][i]=piezas_ne['peon']
